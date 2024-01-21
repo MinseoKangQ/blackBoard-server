8094f5b8781a87f5d580e9163ea8d66eacde1cae
MinseoKangQ <kms02171@naver.com>
2024-01-21 23:10:13 +0900

    :fire: Remove unnecessary things
    Remove main class's code related to EntityManager and META-INF/persistence.xml, because this is not actually used.

--- src/main/java/dev/line4/blackBoard/BlackBoardApplication.java
+++ src/main/java/dev/line4/blackBoard/BlackBoardApplication.java
@@ -8,31 +8,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-//@Import(AppConfig.class)
 public class BlackBoardApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BlackBoardApplication.class, args);
-
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("blackboard");
-        EntityManager em = emf.createEntityManager();
-        EntityTransaction tx = em.getTransaction();
-        tx.begin(); // 트랜잭션 시작
-
-        try {
-
-            tx.commit();
-            System.out.println("try!");
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-            System.out.println("catch!");
-            tx.rollback();
-        } finally {
-            System.out.println("finally!");
-            em.close();
-        }
-        emf.close();
-
     }
 
 }
