f2d4472a593e641c6753150195092ec18941b63b
gahyeon <1000ji1@naver.com>
2024-01-25 01:01:13 +0900

    :hammer: Change checkDuplicate -> check-duplicate

--- src/main/java/dev/line4/blackBoard/blackboard/controller/BlackBoardController.java
+++ src/main/java/dev/line4/blackBoard/blackboard/controller/BlackBoardController.java
@@ -7,7 +7,12 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api")
@@ -33,9 +38,9 @@ public class BlackBoardController {
         return result;
     }
 
-    @GetMapping("checkDuplicate")
-    @ApiOperation(value = "userId 중복 확인", notes="userId를 입력받은 후 호출합니다.")
-    public ResponseEntity<ApiResponse<?>> checkDuplicateUserId(@RequestParam("userId") String userId ) {
+    @GetMapping("check-duplicate")
+    @ApiOperation(value = "userId 중복 확인", notes = "userId를 입력받은 후 호출합니다.")
+    public ResponseEntity<ApiResponse<?>> checkDuplicateUserId(@RequestParam("userId") String userId) {
         ResponseEntity<ApiResponse<?>> result = blackBoardServiceImpl.checkDuplicateUserId(userId);
         return result;
     }
