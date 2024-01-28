72e240455172bdfb53d60d23abb4251fe3d0836a
gahyeon <1000ji1@naver.com>
2024-01-28 22:57:19 +0900

    :sparkles: Process if including profanity when creating a letter

--- src/main/java/dev/line4/blackBoard/filtering/service/FilteringServiceImpl.java
+++ src/main/java/dev/line4/blackBoard/filtering/service/FilteringServiceImpl.java
@@ -2,6 +2,7 @@ package dev.line4.blackBoard.filtering.service;
 
 import com.vane.badwordfiltering.BadWordFiltering;
 import dev.line4.blackBoard.filtering.dto.FilterBlackBoardDto;
+import dev.line4.blackBoard.filtering.dto.FilterLetterDto;
 import dev.line4.blackBoard.utils.response.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -14,13 +15,30 @@ public class FilteringServiceImpl implements FilteringService {
 
     // 비속어 확인
     @Override
-    public ResponseEntity<ApiResponse<?>> filterBlackBoard(FilterBlackBoardDto dto) throws Exception {
+    public ResponseEntity<ApiResponse<?>> filterBlackBoard(FilterBlackBoardDto dto) {
         String title = dto.getBlackboard().getTitle();
         String introduction = dto.getBlackboard().getIntroduction();
         String userId = dto.getBlackboard().getUserId();
 
         // 여기에 비속어 확인 로직 추가
         if (containsProfanity(title) || containsProfanity(introduction) || containsProfanity(userId)) {
+            ApiResponse<String> errorResponse = ApiResponse.createFailWithoutData(HttpStatus.BAD_REQUEST.value(),
+                    "비속어가 포함되어 있습니다.");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+        } else {
+            ApiResponse<String> successResponse = ApiResponse.createSuccessWithoutData(HttpStatus.OK.value(),
+                    "비속어가 포함되어 있지 않습니다.");
+            return ResponseEntity.status(HttpStatus.OK).body(successResponse);
+        }
+    }
+
+    @Override
+    public ResponseEntity<ApiResponse<?>> filterLetter(FilterLetterDto dto) {
+        String nickname = dto.getLetter().getNickname();
+        String content = dto.getLetter().getContent();
+
+        // 여기에 비속어 확인 로직 추가
+        if (containsProfanity(nickname) || containsProfanity(content)) {
             ApiResponse<String> errorResponse = ApiResponse.createFailWithoutData(HttpStatus.BAD_REQUEST.value(),
                     "비속어가 포함되어 있습니다.");
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
