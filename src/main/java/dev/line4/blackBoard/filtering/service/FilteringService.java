2a14a166ec04f39db5fe567fbcdb3bb56c70930b
gahyeon <1000ji1@naver.com>
2024-01-28 22:53:01 +0900

    :sparkles: Add filterLetter function

--- src/main/java/dev/line4/blackBoard/filtering/service/FilteringService.java
+++ src/main/java/dev/line4/blackBoard/filtering/service/FilteringService.java
@@ -1,10 +1,14 @@
 package dev.line4.blackBoard.filtering.service;
 
 import dev.line4.blackBoard.filtering.dto.FilterBlackBoardDto;
+import dev.line4.blackBoard.filtering.dto.FilterLetterDto;
 import dev.line4.blackBoard.utils.response.ApiResponse;
 import org.springframework.http.ResponseEntity;
 
 public interface FilteringService {
 
     ResponseEntity<ApiResponse<?>> filterBlackBoard(FilterBlackBoardDto dto) throws Exception;
+
+    ResponseEntity<ApiResponse<?>> filterLetter(FilterLetterDto dto) throws Exception;
+
 }
