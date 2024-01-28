fe3f2ea05f5ca66f0eacf7b99cbb60a7fee17643
gahyeon <1000ji1@naver.com>
2024-01-29 00:35:19 +0900

    :recycle: Wrap with a Req static class

--- src/main/java/dev/line4/blackBoard/filtering/dto/FilterBlackBoardDto.java
+++ src/main/java/dev/line4/blackBoard/filtering/dto/FilterBlackBoardDto.java
@@ -1,19 +1,22 @@
 package dev.line4.blackBoard.filtering.dto;
 
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@Getter
-@NoArgsConstructor
 public class FilterBlackBoardDto {
 
-    private BlackBoard blackboard;
-
     @Getter
-    @NoArgsConstructor
-    public static class BlackBoard {
-        private String title;
-        private String introduction;
-        private String userId;
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    public static class Req {
+        private BlackBoard blackboard;
+
+        @Getter
+        @NoArgsConstructor(access = AccessLevel.PROTECTED)
+        public static class BlackBoard {
+            private String title;
+            private String introduction;
+            private String userId;
+        }
     }
 }
