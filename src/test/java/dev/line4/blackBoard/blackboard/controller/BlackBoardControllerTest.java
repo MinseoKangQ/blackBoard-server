94fe6bece11f61aa118d61d66e3dbb3e039962d4
MinseoKangQ <kms02171@naver.com>
2024-02-05 15:26:37 +0900

    :white_check_mark: Add addBlackBoardTest API test code

--- src/test/java/dev/line4/blackBoard/blackboard/controller/BlackBoardControllerTest.java
+++ src/test/java/dev/line4/blackBoard/blackboard/controller/BlackBoardControllerTest.java
@@ -11,6 +11,8 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import javax.transaction.Transactional;
+
 @SpringBootTest
 @AutoConfigureMockMvc
 public class BlackBoardControllerTest {
@@ -20,9 +22,53 @@ public class BlackBoardControllerTest {
 
     @Test
     @DisplayName("칠판 개수 조회 - 성공")
-    void countBlackBoardsTest () throws Exception {
+    void countBlackBoardsTestSuccess () throws Exception {
         mvc.perform(MockMvcRequestBuilders.get("/api/blackboards").contentType(MediaType.APPLICATION_JSON))
                 .andDo(MockMvcResultHandlers.print())
                 .andExpect(MockMvcResultMatchers.status().isOk());
     }
+
+    @Test
+    @DisplayName("칠판 등록 - 성공")
+    @Transactional // 테스트 완료 후 rollback
+    void addBlackBoardTestSuccess() throws Exception {
+
+        // JSON 데이터 문자열
+        String jsonContent = "{"
+                + "\"blackboard\" : {"
+                + "    \"title\" : \"칠판 안녕\","
+                + "    \"introduction\" : \"칠판 소개\","
+                + "    \"userId\" : \"testttttttttttttttttttttt\","
+                + "    \"openDate\" : \"2024-02-20T18:00:00\""
+                + "},"
+                + "\"stickers\" : ["
+                + "    {"
+                + "        \"num\" : 1,"
+                + "        \"positionX\" : 0.1,"
+                + "        \"positionY\" : 0.2,"
+                + "        \"img\" : 3,"
+                + "        \"width\" : 1.5,"
+                + "        \"angle\" : 0.5,"
+                + "        \"mirror\" : -1"
+                + "    },"
+                + "    {"
+                + "        \"num\" : 2,"
+                + "        \"positionX\" : 0.3,"
+                + "        \"positionY\" : 0.4,"
+                + "        \"img\" : 7,"
+                + "        \"width\" : 1.2,"
+                + "        \"angle\" : 0.7,"
+                + "        \"mirror\" : 1"
+                + "    }"
+                + "]"
+                + "}";
+
+        // POST 요청 구성 및 실행
+        mvc.perform(MockMvcRequestBuilders.post("/api/blackboard")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(jsonContent)) // JSON 데이터 요청 본문에 포함
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isOk());
+    }
+
 }
