b6360deb3085cdfeeea3e7029ad2d0080e852236
MinseoKangQ <kms02171@naver.com>
2024-02-05 15:10:29 +0900

    :white_check_mark: Add countBlackBoards API test code

---
+++ src/test/java/dev/line4/blackBoard/blackboard/controller/BlackBoardControllerTest.java
@@ -0,0 +1,28 @@
+package dev.line4.blackBoard.blackboard.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class BlackBoardControllerTest {
+
+    @Autowired
+    MockMvc mvc;
+
+    @Test
+    @DisplayName("칠판 개수 조회 - 성공")
+    void countBlackBoardsTest () throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/api/blackboards").contentType(MediaType.APPLICATION_JSON))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isOk());
+    }
+}
